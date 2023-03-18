45392dc3060aa46eed5bf75d5adafcf062e5a73f
Mgarcia456 <Asvp_miguel36@icloud.com>
2023-03-18 10:52:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Form from './components/Form';
+import PeopleComponent from './components/PeopleComponent';
+import PlanetComponent from './components/PlanetComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <h1>Luke API Walker</h1>
+    <Form/>
+    <Routes>
+      <Route path='/people/:id' element={<PeopleComponent />} />
+      <Route path='/planets/:id' element={<PlanetComponent />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+const Form = (props) =>{
+
+    const [type, setType] = useState("people")
+    const [id, setId] = useState(1)
+    const nav = useNavigate()
+
+    const search = (e) => {
+        e.preventDefault()
+        nav(`/${type}/${id}`)
+    }
+
+    return (
+        <form onSubmit={search}>
+            <label>Search For</label>
+            <select onChange={e=> setType(e.target.value)} value={type} >
+                <option value="people">People</option>
+                <option value="planets">Planets</option>
+            </select>
+            <label>Id:</label>
+            <input type="number" value={id} min={1} onChange={e=> setId(e.target.value)} />
+            <input type="submit" value="Search" />
+        </form>
+    )
+}
+export default Form
